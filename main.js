0dcabaeb70749bce93c6618a81bf373b9d10ab95
Marc Ciruelos <mciruelos@multiestetica.com>
2021-04-22 23:39:48 +0200

    Blue socks with v-bind (vue3)

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const app = Vue.createApp({
     data() {
         return {
             product: 'Socks',
-            image: './assets/images/socks_green.jpg'
+            images: './assets/images/socks_green.jpg'
         }
     }
 })
